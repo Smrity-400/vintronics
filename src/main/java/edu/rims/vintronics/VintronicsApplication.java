5393ec0389c0a2810907d1e27f250695485e751f
smrity-400 <smrity2002reddy@gmail.com>
2025-01-18 11:43:07 +0530

    Initial env setup

---
+++ src/main/java/edu/rims/vintronics/VintronicsApplication.java
@@ -0,0 +1,13 @@
+package edu.rims.vintronics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VintronicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VintronicsApplication.class, args);
+	}
+
+}
