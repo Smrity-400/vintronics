643a19e7bfea68ac837a8daf1bf3652bea1b4aac
Suhasini-140 <padhisuhasini098@gmail.com>
2025-01-22 08:48:04 +0530

    design

--- src/main/java/edu/rims/vintronics/controller/HomeController.java
+++ src/main/java/edu/rims/vintronics/controller/HomeController.java
@@ -1,5 +1,17 @@
 package edu.rims.vintronics.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+@RequestMapping("/customer")
 public class HomeController {
+    @GetMapping({"/home", "/"})
+     String home() {
+        return "customer/home";
+    }
     
 }
