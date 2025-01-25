bfec46a2b4c7d9e312b173ae7a7d423736324130
smrity-400 <smrity2002reddy@gmail.com>
2025-01-25 11:06:41 +0530

    add images

--- src/main/java/edu/rims/vintronics/controller/HomeController.java
+++ src/main/java/edu/rims/vintronics/controller/HomeController.java
@@ -4,8 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
-
 @Controller
 @RequestMapping("/customer")
 public class HomeController {
