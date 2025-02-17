7d5d0a90fb29c69e3c1a7cbd4face251fda5fa20
smrity-400 <smrity2002reddy@gmail.com>
2025-02-17 23:12:16 +0530

    plp page design

--- src/main/java/edu/rims/vintronics/controller/ProductController.java
+++ src/main/java/edu/rims/vintronics/controller/ProductController.java
@@ -1,5 +1,16 @@
 package edu.rims.vintronics.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequestMapping("/customer")
 public class ProductController {
+    @GetMapping({"/product", "/"})
+     String product() {
+        return "customer/product";
+    }
+    
     
 }
